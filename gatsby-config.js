1d121fc284578389664e12ae28ca44c2196e52ff
zy <teezhengyang16@gmail.com>
2021-08-20 11:58:59 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "stonks",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
+};
