cd4b719cd78ad42ab6c9306cdb1488d6451b1b35
zy <teezhengyang16@gmail.com>
2021-11-19 14:30:21 +0800

    add header

---
+++ src/pages/alltime.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import MainLayout from "../templates/MainLayout";
+
+const AllTimePage = ({ location }) => {
+  return <MainLayout path={location.pathname}>Coming soon.</MainLayout>;
+};
+
+export default AllTimePage;
